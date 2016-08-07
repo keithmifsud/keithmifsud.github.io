72bb3438b495e547ec439d324625b62e80e3c6a5
Keith Mifsud <mifsud.k@gmail.com>
2016-08-07 13:58:53 +0200

    Added Roboto font

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,10 @@ var env         = require('minimist')(process.argv.slice(2)),
 	koutoSwiss  = require('kouto-swiss'),
 	prefixer    = require('autoprefixer-stylus'),
 	imagemin    = require('gulp-imagemin'),
-	cp          = require('child_process');
+	cp          = require('child_process'),
+    replace     = require('gulp-replace'),
+    postcss     = require('gulp-postcss');
+
 
 var messages = {
 	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -71,6 +74,37 @@ gulp.task('js', function(){
 		.pipe(gulp.dest('assets/js/'));
 });
 
+
+/**
+ * Fonts Task
+ */
+
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/*.css').pipe(
+        postcss([
+            require('postcss-font-magician')({ /* options */})
+        ])
+    ).pipe(
+        gulp.dest('assets/css/')
+    );
+});
+
+
+/*gulp.task('font-css', function () {
+    return gulp.src('src/fonts/!**!/!*.css')
+        .pipe(plumber())
+        .pipe(concat('fonts.css'))
+        .pipe(replace("url('", "url('../fonts/"))
+        .pipe(gulp.dest('assets/css/'))
+});*/
+
+/*gulp.task('fontgen', function() {
+    return gulp.src('src/fonts/!**!/!*.{ttf, otf}')
+        .pipe(fontgen({
+            dest: "assets/css/"
+        }));
+});*/
+
 /**
  * Imagemin Task
  */
@@ -87,6 +121,7 @@ gulp.task('imagemin', function() {
  */
 gulp.task('watch', function () {
 	gulp.watch('src/styl/**/*.styl', ['stylus']);
+	gulp.watch('src/fonts/**/*.css', ['fonts']);
 	gulp.watch('src/js/**/*.js', ['js']);
 	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
 	gulp.watch(['**/*.html','index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
@@ -96,4 +131,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the stylus,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+gulp.task('default', ['js', 'stylus', 'fonts', 'browser-sync', 'watch']);
